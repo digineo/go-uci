0f6c7830780ac532e3a11e4311b62a72c8b86c2f
KnKay <kay.richter@googlemail.com>
2023-01-30 21:25:42 +0100

    implemented scp download

--- uci_ssh.go
+++ uci_ssh.go
@@ -1,10 +1,14 @@
 package uci
 
 import (
-	"bytes"
-	"log"
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/tmc/scp"
@@ -38,16 +42,43 @@ func (t *SshTree) Disonnect() (err error) {
 func (t *SshTree) loadConfig(name string) (err error) {
 	session, err := t.client.NewSession()
 	defer session.Close()
-	session.Stderr = os.Stderr
-	var body bytes.Buffer
-	session.Stdout = &body
-	path := filepath.Join(DefaultTreePath, name)
-	err = session.Run("cat " + path)
+
+	out, err := session.StdoutPipe()
 	if err != nil {
-		log.Fatalln("Unable to run command: " + err.Error())
+		return
 	}
 
-	cfg, err := parse(name, body.String())
+	in, err := session.StdinPipe()
+	if err != nil {
+		return
+	}
+	defer in.Close()
+
+	// Start the request
+	path := filepath.Join(DefaultTreePath, name)
+	err = session.Start(fmt.Sprintf("scp -qf %q", path))
+	if err != nil {
+		return
+	}
+	err = sendAck(in)
+	if err != nil {
+		return
+	}
+
+	// Get file header
+	file, size, err := parseGetHeader(out)
+	if !strings.Contains(file, name) {
+		return errors.New("Header error")
+	}
+
+	// Get data
+	sendAck(in)
+	buffer := make([]byte, size)
+	read, err := out.Read(buffer)
+	if read != size {
+		return errors.New("some strange read error")
+	}
+	cfg, err := parse(name, string(buffer))
 	if err != nil {
 		return err
 	}
@@ -110,5 +141,40 @@ func (t *SshTree) saveConfig(c *config) (err error) {
 	err = scp.CopyPath(f.Name(), destinationPath, session)
 
 	c.tainted = false
+	return
+}
+
+func sendAck(writer io.Writer) (err error) {
+	var msg = []byte{0}
+	n, err := writer.Write(msg)
+	if err != nil {
+		return err
+	}
+	if n < len(msg) {
+		return errors.New("failed to write ack buffer")
+	}
+	return nil
+}
+
+func parseGetHeader(reader io.Reader) (file string, size int, err error) {
+	buffer := make([]uint8, 1)
+	_, err = reader.Read(buffer)
+	if err != nil {
+		return
+	}
+
+	if buffer[0] > 0 {
+		bufferedReader := bufio.NewReader(reader)
+		response, err := bufferedReader.ReadString('\n')
+		parsed := strings.Split(response, " ")
+		file = strings.Replace(parsed[2], "\n", "", 1)
+		if err != nil {
+			return "", 0, err
+		}
+		size, err = strconv.Atoi(parsed[1])
+		if err != nil {
+			return "", 0, err
+		}
+	}
 	return
 }
